6c755f9eada08ac0603157b902a69025a8cef747
Lara Sakanaka <85855158+LaraYuka@users.noreply.github.com>
2021-08-26 16:30:06 +0000

    Service Work - 26/8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
       // Registration was successful
       console.log('O registro do ServiceWorker foi bem-sucedido com o escopo: ', registration.scope);
     }, function(err) {
